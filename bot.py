5c97ac8f2596cab16a066eafa96471ef43bf791d
James <51536154+mbruhyo@users.noreply.github.com>
2021-01-28 23:21:11 +0000

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,24 @@
+import cmyui
+import glob
+import discord
+import random
+import string
+
+class itekiBot(discord.Client):
+    async def on_ready(self):
+        print(f'Logged in as {self.user}')
+
+    async def on_message(self, message):
+        db = cmyui.AsyncSQLPool()
+        await db.connect(glob.config.mysql)
+        if message.author.id == self.user.id:
+            return
+        
+        if message.content.startswith('!generate') and message.channel.id == glob.config.generate_channel:
+            key = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(20))
+            await db.execute(f'INSERT INTO beta_keys(beta_key, generated_by) VALUES ("{key}", "{message.author}")')
+            await message.delete()
+            return await message.author.send(f'Key generated!\n\nKey: `{key}`')
+
+client = itekiBot()
+client.run(glob.config.token)
---
+++ config.sample.py
@@ -0,0 +1,9 @@
+mysql = {
+    'db': 'ape',
+    'host': 'localhost',
+    'password': 'ape',
+    'user': 'ape'
+}
+
+token = ''
+generate_channel = ''
